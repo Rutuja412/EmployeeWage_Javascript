bccaa4c59aedd691a2191f5cb104d3b6f19996c1
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-02 09:16:11 +0530

    [Rutuja]:Calculate Wages For Month

--- Employee.js
+++ Employee.js
@@ -4,14 +4,19 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 2;
+const NUM_OF_WORKING_DAYS = 20;
 let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;//check for attendance
 empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log(" Emp Wage: " + empWage);
+console.log( "Total Hrs: "+ empHrs+" Emp Wage: " + empWage);
+for (let day =0;day<NUM_OF_WORKING_DAYS; day++)
+{
+    empHrs += getWorkingHours(empCheck)
+}
     
 function getWorkingHours(empCheck) {
+
     switch (empCheck) {
         case IS_PART_TIME:
             return PART_TIME_HOURS;
