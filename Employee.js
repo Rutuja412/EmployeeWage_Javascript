61a06a747df4dcf68db1027651774ce1af6f3bcd
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-02 08:56:37 +0530

    [Rutuja]:Calculate Daily Wage For Daily working hrs

--- Employee.js
+++ Employee.js
@@ -1,25 +1,24 @@
-//UC2-Employee Daily Wage Calculation
-
+//UC3-Refactor uc2 to cal employee wage using Function
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
-const WAGE_PER_HR = 20;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
-let empCheck = Math.floor(Math.random() * 3 );  // check for attendence
-let empWage, workingHours;          //variables
-switch(empCheck){
-    case IS_FULL_TIME:
-        workingHours = FULL_TIME_HOURS;
-        console.log("Employee is present for full-time");
-        break;
-    case IS_PART_TIME:
-        workingHours = PART_TIME_HOURS;
-        console.log("Employee is Present for part-time");
-        break;
-    default:
-        console.log("Employee is Absent");
-        workingHours = 0;
-        break;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 2;
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;//check for attendance
+empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log(" Emp Wage: " + empWage);
+    
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-empWage = workingHours * WAGE_PER_HR;
-console.log("Employee Wage: " + empWage);
+
